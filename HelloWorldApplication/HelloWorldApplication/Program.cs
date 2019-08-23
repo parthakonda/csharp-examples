7a3a37db8c5dd86a41435ad585a1a1c36c8d412b
partha_aptuz <partha@aptuz.com>
2019-08-23 15:30:23 +0530

    initial commit

---
+++ HelloWorldApplication/HelloWorldApplication/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HelloWorldApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
