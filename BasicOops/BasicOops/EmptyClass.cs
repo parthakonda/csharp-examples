a753abc2d5684023125aeea4471e2d9c09fa35b0
parthakonda <partha@aptuz.com>
2019-08-23 16:40:26 +0530

    added data types

---
+++ BasicOops/BasicOops/EmptyClass.cs
@@ -0,0 +1,30 @@
+﻿using System;
+namespace BasicOops
+{
+    public class AllDataTypes
+    {
+        public void EmptyClass()
+        {
+            bool b = false;
+            byte by = 1;
+            char ch = 'a';
+            int i = -1;
+            short sh = -2;
+            long lg = -3;
+            uint ui = 1;
+            ushort us = 2;
+            sbyte sb = -2;
+
+            Console.WriteLine(b);
+            Console.WriteLine(by);
+            Console.WriteLine(ch);
+            Console.WriteLine(i);
+            Console.WriteLine(sh);
+            Console.WriteLine(lg);
+            Console.WriteLine(ui);
+            Console.WriteLine(us);
+            Console.WriteLine(sb);
+
+        }
+    }
+}
