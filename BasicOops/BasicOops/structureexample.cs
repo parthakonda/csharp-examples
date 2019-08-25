a88eaf8d6fa1c9937c927524742862b0bda85769
parthakonda <partha@aptuz.com>
2019-08-25 13:36:40 +0530

    added structure

---
+++ BasicOops/BasicOops/structureexample.cs
@@ -0,0 +1,26 @@
+﻿using System;
+namespace BasicOops
+{
+    public class structureexample
+    {
+        struct Employee
+        {
+            public string name;
+            public int employeeid;
+            public string description;
+        }
+        
+
+        public void Display()
+        {
+            Employee partha = new Employee();
+            partha.name = "Partha Saradhi";
+            partha.employeeid = 123;
+            partha.description = "new employee";
+
+            Console.WriteLine("name of the employee: {0}", partha.name);
+            Console.WriteLine("empid of the employee: {0}", partha.employeeid);
+            Console.WriteLine("description of the employee: {0}", partha.description);
+        }
+    }
+}
