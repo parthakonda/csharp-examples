117e3e3799ef1bae1b31ebc2b1cd5af16e736528
Partha Saradhi <parthasaradhi1992@gmail.com>
2019-08-25 12:06:16 +0530

    added strings

---
+++ BasicOops/BasicOops/StringsExample.cs
@@ -0,0 +1,21 @@
+﻿using System;
+namespace BasicOops
+{
+    public class StringsExample
+    {
+        // defining string
+        string first_name = "Partha";
+        string last_name = "Saradhi";
+
+        public string getFullName()
+        {
+            string temp = first_name + " " + last_name;
+            return temp;
+        }
+
+        public void Display()
+        {
+            Console.WriteLine(getFullName());
+        }
+    }
+}
